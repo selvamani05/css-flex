fed72c0ab11a7cb026bd556de0bd7670e4f4faea
selvamani05 <selvamani05@ymail.com>
2020-09-10 22:45:55 +0530

    JS-6

--- Java Script/script.js
+++ Java Script/script.js
@@ -10,5 +10,26 @@ alert(1 / 0);
 
 alert(`the result is ${1 + 2}`); // the result is 3 */
 
-let isGreater = 34 > 36;
-alert(isGreater); // true (the comparison result is "yes")
+/* /* let isGreater = 34 > 36;
+alert(isGreater); // true (the comparison result is "yes") */
+/* 
+let name = "Ilya";
+
+alert(`hello ${1}`); // ?
+
+alert(`hello ${"name"}`); // ?
+
+alert(`hello ${name}`); // ? */
+*
+/
+
+/* 
+let age = prompt('How old are you?', 100);
+
+alert(`You are ${age} years old!`); // You are 100 years old! */
+
+/* var _lastName = "johnson";
+var _x = "2";
+var _100 = "5": */
+
+alert("10" * "5");
