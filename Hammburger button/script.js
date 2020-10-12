c7195d68557f48fc68615c553364df1e6db323dd
selvamani05 <selvamani05@ymail.com>
2020-10-12 15:59:07 +0530

    JS-Hamburger-button

---
+++ Hammburger button/script.js
@@ -0,0 +1,11 @@
+const hamburger_icon = document.getElementById('hamburger_icon');
+const nav = document.getElementById('nav');
+
+hamburger_icon.addEventListener('click'.() => {
+    nav.classList.toggle('active');
+    hamburger_icon_active.classList.toggle('active');
+})
+
+function myFunction() {
+    getElementById = nav
+}
