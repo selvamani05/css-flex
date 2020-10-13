a7f545457748966d00c1875d2a2f2571927751b1
selvamani05 <selvamani05@ymail.com>
2020-10-13 21:34:50 +0530

    JS-Background-changer-1

--- JS Purple Heart Rain/script.js
+++ JS Purple Heart Rain/script.js
@@ -1,12 +1,12 @@
 function createHeart() {
     const heart = document.createElement("div");
-    heart.classList.add("heart");
+    /* heart.classList.add("heart"); */
 
     heart.style.left = Math.random() * 100 + "vw";
 
     heart.style.animationDuration = Math.random() * 2 + 3 + "s";
 
-    heart.innerText = "❤️";
+    heart.innerText = "❤";
 
     document.body.appendChild(heart);
 
