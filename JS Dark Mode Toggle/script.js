f58b7d3a0ba31827f446a8e06f5bc8f81852a58d
selvamani05 <selvamani05@ymail.com>
2020-10-14 13:41:03 +0530

    JS-Image-Carousel-1

--- JS Dark Mode Toggle/script.js
+++ JS Dark Mode Toggle/script.js
@@ -1,5 +1,5 @@
 const toggle = document.getElementById("toggle");
 
-toggle.addEventListener("change", (event) => {
-    document.body.classList.toggle("dark", event.target.checked);
+toggle.addEventListener("change", (e) => {
+    document.body.classList.toggle("dark", e.target.unchecked);
 });
