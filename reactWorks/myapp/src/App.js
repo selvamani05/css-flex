7eaeed84d632d62315000078f2aa862380a753bb
selvamani05 <selvamani05@ymail.com>
2020-10-23 22:26:53 +0530

    React-test-01

---
+++ reactWorks/myapp/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Ninjas from './Ninjas';
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>My First React App</h1>
+     <p>Welcome!!!</p>
+     <Ninjas />
+
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ reactWorks/myapp/src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component{
+    render(){
+        return(
+            <div className="Ninja">
+                <div>Name: Ryu</div>
+                <div>Age: 50</div>
+                <div>Belt: BlackBelt</div>
+            </div>
+        )
+    }
+}
+
+export default Ninjas
