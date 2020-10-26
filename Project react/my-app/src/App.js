da87efdbf5fa7235c01db4622c1ec4458a14418c
selvamani05 <selvamani05@ymail.com>
2020-10-26 20:34:12 +0530

    React-Nesting-Files

---
+++ Project react/my-app/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+
+ class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hai</h1>
+        <Ninjas />
+      </div>
+    )
+  }
+}
+
+
+
+
+export default App;
