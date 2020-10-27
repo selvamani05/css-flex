1aa359268b6ea58cb5f53c95546cb8df7b9769e1
selvamani05 <selvamani05@ymail.com>
2020-10-27 16:44:38 +0530

    React-Nesting-Outputting-Lists

--- Project react/my-app/src/App.js
+++ Project react/my-app/src/App.js
@@ -1,21 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Ninjas from './Ninjas';
 
- class App extends Component {
+
+class App extends Component {
+  state = {
+    ninjas : [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Rome', age: 25, belt: 'blue', id: 2 },
+      { name: 'Regal', age: 45, belt: 'benn', id: 3 },
+
+    ]
+
+  }
   render() {
     return (
-      <div>
-        <h1>Hai</h1>
-        <Ninjas name="Ryu" age="25" belt="gren" />
-
-
-        <Ninjas name="rtan" age="25" belt="gren" />
+      <div className="App">
+      <h1>My react app</h1>
+      <p>Welcome !!!</p>
+      <Ninjas ninjas={this.state.ninjas}/>
+        
       </div>
     )
   }
 }
-
-
 
 
 export default App;
