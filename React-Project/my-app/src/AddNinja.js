a8737dd189610ebb2b4759f974e427869068cbc0
selvamani05 <selvamani05@ymail.com>
2020-11-01 22:28:47 +0530

    React-Form-Revisited-01

---
+++ React-Project/my-app/src/AddNinja.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class AddNinja extends Component {
+    render() {
+        return (
+            <div>
+                <form>
+                
+
+
+
+
+                </form>
+            </div>
+        )
+    }
+}
