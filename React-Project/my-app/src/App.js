5c9b09ad7cd42511f9713f14f29c48fdc49b1f13
selvamani05 <selvamani05@ymail.com>
2020-11-03 12:44:25 +0530

    React-Form-data-CSS

--- React-Project/my-app/src/App.js
+++ React-Project/my-app/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   state = {
     ninjas: [
       { name: 'Ryu', age: 30, belt: 'black', id: 1 },
-      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Yay', age: 20, belt: 'green', id: 2 },
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
