f3b4e2382f661073966fd1045fcc262351d922a5
selvamani05 <selvamani05@ymail.com>
2020-11-03 22:44:43 +0530

    React-Todo-List

---
+++ To-Do-List/todoapp/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import todos from './todos'
+
+class App extends Component {
+  state = [
+    {id: 1, content: 'buy some milk'}
+    {id: 2, content: 'play mario kart'}
+  ]
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    )
+  }
+}
---
+++ To-Do-List/todoapp/src/todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const Todos = () => {
+    return (
+        <div className="todos collection">
+        {id: 1,content: 'buy some milk'}
+        {id:2, content: 'play mario kart'
+
+
+
+        </div>
+    )
+}
