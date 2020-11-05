cc200a5318413db9d3e6b0189c6a776e8a43332d
selvamani05 <selvamani05@ymail.com>
2020-11-05 18:34:58 +0530

    React-Router-01

---
+++ Poketimes/poketimes/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Navbar from './Components/Navbar'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    )
+  }
+}
+export default App
---
+++ Poketimes/poketimes/src/Components/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const About = () => {
+    return (
+        <div className="container">
+        <h4 className="center">About</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,  </p>
+
+        </div>
+    )
+}
+
+export default About
