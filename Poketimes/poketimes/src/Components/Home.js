7d6caee730149f4059f75b2914c25da8769c4f4c
selvamani05 <selvamani05@ymail.com>
2020-11-05 18:35:43 +0530

    React-Router-03

---
+++ Poketimes/poketimes/src/Components/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+        <h4 className="center">Home</h4>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,  </p>
+
+        </div>
+    )
+}
+
+export default Home
