cc33db66d8640a4bd802409f6b9bb1e796f1d85a
selvamani05 <selvamani05@ymail.com>
2020-11-10 13:13:24 +0530

    React-Hooks-useState-with-object

--- React Hooks/my-app/src/App.js
+++ React Hooks/my-app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import ClassCounter from './Components/ClassCounter'
 import HookCounter from './Components/HookCounter';
+import HookCounter3 from './Components/HookCounter3';
 
 
 function App() {
   return (
     <div className="App">
-      <HookCounter />
+      <HookCounter3 />
     </div>
   );
 }
