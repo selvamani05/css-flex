561f0c3359a65de568bed6ca11662e0179ded594
selvamani05 <selvamani05@ymail.com>
2020-11-10 14:56:24 +0530

    React-Hooks-Run-effects-only-once

--- React Hooks/my-app/src/App.js
+++ React Hooks/my-app/src/App.js
@@ -4,13 +4,15 @@ import HookCounter from './Components/HookCounter';
 import HookCounter3 from './Components/HookCounter3';
 import HookCounter4 from './Components/HookCounter4';
 import HookCounter5 from './Components/HookCounter5';
+import HookMouse from './Components/HookMouse';
 
 
 function App() {
   return (
     <div className="App">
     {/* <ClassCounter /> */}
-      <HookCounter5 />
+     {/*  <HookCounter5 /> */}
+     <HookMouse />
     </div>
   );
 }
