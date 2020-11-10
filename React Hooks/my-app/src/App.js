1a9c072152c87f2f44cd232a995c607417995818
selvamani05 <selvamani05@ymail.com>
2020-11-10 12:37:55 +0530

    React-Hooks-useState-with-previous-state

--- React Hooks/my-app/src/App.js
+++ React Hooks/my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ClassCounter from './Components/ClassCounter'
+import HookCounter from './Components/HookCounter';
+
 
 function App() {
   return (
     <div className="App">
-      <ClassCounter />
+      <HookCounter />
     </div>
   );
 }
