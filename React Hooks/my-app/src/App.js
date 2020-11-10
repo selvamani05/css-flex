b62fc9a309e212a51191f1371a583a9793f68851
selvamani05 <selvamani05@ymail.com>
2020-11-10 13:45:13 +0530

    React-Hooks-useState-with-array

--- React Hooks/my-app/src/App.js
+++ React Hooks/my-app/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import ClassCounter from './Components/ClassCounter'
 import HookCounter from './Components/HookCounter';
 import HookCounter3 from './Components/HookCounter3';
+import HookCounter4 from './Components/HookCounter4';
 
 
 function App() {
   return (
     <div className="App">
-      <HookCounter3 />
+      <HookCounter4/>
     </div>
   );
 }
