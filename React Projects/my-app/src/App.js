4a0bf8a6cbc9fa427ca7f5cd341ff58ea446a747
selvamani05 <selvamani05@ymail.com>
2020-11-23 19:15:51 +0530

    React-Random-Number-Generator-using-hooks-22

--- React Projects/my-app/src/App.js
+++ React Projects/my-app/src/App.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react'
 
 function App() {
   const [minVal,setMinVal] = useState(0);
- /*  const [maxVal,setMaxVal] = useState(10); */
+  const [maxVal,setMaxVal] = useState(10);
   const [randomNum,setRandomNum] = useState(5);
 
   /* const handleRandomNum = () => {
@@ -24,8 +24,8 @@ function App() {
       <input type="number" value={minVal} onChange={e => setMinVal(+e.target.value)}/>
     </div>
     
-      {/* <p>Max:</p>
-      <input type="number" value={maxVal} onChange={e => setMaxVal(+e.target.value)}/> */}
+      <p>Max:</p>
+      <input type="number" value={maxVal} onChange={e => setMaxVal(+e.target.value)}/>
     </div>
     {/* <button onClick={handleRandomNum}>Get Random Number</button> */}
 
