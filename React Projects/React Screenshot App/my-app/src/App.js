898db5d98dabbe423f9ccba7db473b27d85b421c
selvamani05 <selvamani05@ymail.com>
2020-11-25 17:43:35 +0530

    React-Screenshot-App-01

---
+++ React Projects/React Screenshot App/my-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="App">
+      <nav>
+        <div className="container">
+          <form>
+          <input type="text"/>
+          <button type="submit">Take Screenshot</button>
+          </form>
+        </div>
+      </nav>
+
+      <div className="hero">
+
+
+        
+      </div>
+
+    </div>
+  )
+}
+
+export default App
