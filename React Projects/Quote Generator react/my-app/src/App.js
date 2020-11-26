a49b0c62354410d19f782109ae80b8ac5bcfd162
selvamani05 <selvamani05@ymail.com>
2020-11-26 20:41:26 +0530

    React-Quote-Generator

---
+++ React Projects/Quote Generator react/my-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+ class App extends Component {
+  render() {
+    return (
+      <div>
+        heLLO
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ React Projects/Quote Generator react/my-app/src/Stock.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from './'
+
+function Stock() {
+    return (
+        <div>
+          
+        </div>
+    )
+}
+
+export default Stock
