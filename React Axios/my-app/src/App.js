fe2b224daf1aac664f103f1037a25f71c66eda89
selvamani05 <selvamani05@ymail.com>
2020-11-30 22:17:52 +0530

    React-Axios

---
+++ React Axios/my-app/src/App.js
@@ -0,0 +1,17 @@
+import Userform from './components/Userform'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">HTTP Calls in React</h1>
+        
+        
+      </header>
+      <Userform/>
+    </div>
+  );
+}
+
+export default App;
---
+++ React Axios/my-app/src/components/Userform.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Userform = () => {
+    return (
+        <div>
+           <form>
+               <input style={{margin:"20px auto",display:"block"}} type="text" name="username" />
+               <button>Submit</button>
+           </form> 
+        </div>
+    )
+}
+export default Userform
