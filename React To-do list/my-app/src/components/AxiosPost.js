9c188556d2317243b189db2bd489de7b2ce29e1d
selvamani05 <selvamani05@ymail.com>
2020-12-04 22:39:27 +0530

    React-axios-3

---
+++ React To-do list/my-app/src/components/AxiosPost.js
@@ -0,0 +1,49 @@
+import React,{useState} from 'react';
+import axios from 'axios';
+
+function AxiosPost() {
+
+
+    const [firstName,setFirstName] = useState('');
+    const [lastName,setLastName] = useState('');
+
+    function postCall(event) {
+        event.preventDefault();
+
+        if (firstName != "" && lastName != "") {
+            axios.post('https://jsonplaceholder.typicode.com/posts', {
+                firstName: firstName,
+                lastName: lastName
+              })
+              .then(function (response) {
+                alert('Thanks for the data');
+                setFirstName('')
+                setLastName ('')
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+            
+        } else {
+            alert('Give non empty values')
+        }
+
+        
+        
+    }
+    return (
+        <div>
+            <button onClick={postCall}> POST CALL </button>
+            <form onSubmit={postCall}>
+            <input type='text'  value={firstName} onChange={e => {setFirstName(e.target.value)}}/>
+                <input type='text' value={lastName} onChange={e => {setLastName(e.target.value)}}/>
+                <input type="submit" value="Submit" />
+                
+            </form>
+
+        </div>
+    )
+}
+
+export default AxiosPost
+
