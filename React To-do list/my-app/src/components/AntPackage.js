05fac3433322a911316e062cb9cedeb958df00ef
selvamani05 <selvamani05@ymail.com>
2020-12-04 22:40:09 +0530

    ANT-package

---
+++ React To-do list/my-app/src/components/AntPackage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { DatePicker } from 'antd';
+
+function AntPackage() {
+    return (
+        <div>
+            <DatePicker />
+            <div>
+        <input type="date"/>
+
+            </div>
+        </div>
+    )
+}
+
+export default AntPackage
