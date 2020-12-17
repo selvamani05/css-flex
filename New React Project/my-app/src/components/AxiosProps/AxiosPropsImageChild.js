9eab5b7d6aac2fdff5f997c03fce7f8e40519e97
selvamani05 <selvamani05@ymail.com>
2020-12-17 16:53:18 +0530

    react-useEffect-Axios-Array-destructuring

--- New React Project/my-app/src/components/AxiosProps/AxiosPropsImageChild.js
+++ New React Project/my-app/src/components/AxiosProps/AxiosPropsImageChild.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 function AxiosPropsImageChild(props) {
     return (
-        <div>
+        <div className="individual-card">
+        <div className="card-contents">
             <p>Album id: {props.data.albumId}</p>
             <p>Id : {props.data.id}</p>
             <p>Title: {props.data.title}</p>
-            <p>URL:  {props.data.url} </p>
+            <p className="card-url">URL:  {props.data.url} </p>
            {/*  <p>ThumbnailUrl: {props.data.thumbnailUrl} </p> */}
             {/* <a href={props.data.thumbnailUrl}>ThumbnailUrl</a> */}
-            <img src={props.data.thumbnailUrl} alt="Thumbnail"/>
-            
+            <img className="card-image" src={props.data.thumbnailUrl} alt="Thumbnail"/>
+            </div>
         </div>
     )
 }
--- New React Project/my-app/src/components/AxiosProps/AxiosPropsImagesParent.js
+++ New React Project/my-app/src/components/AxiosProps/AxiosPropsImagesParent.js
@@ -1,20 +1,29 @@
 import React,{useState,useEffect} from 'react'
 import axios from "axios";
 import AxiosPropsImageChild from "./AxiosPropsImageChild";
+import _ from 'lodash';
+
 
 function AxiosPropsImagesParent(props) {
 
 const [data, setData] = useState([]);
+const [chunks, setChunks]= useState([]);
 
 useEffect(() => {
     getcall()
     }, [])
+
+
+    useEffect(() => {    
+        setChunks(splitArray(data))       
+    }, [data])
 
 function getcall() {
     axios.get('https://jsonplaceholder.typicode.com/photos')
   .then(function (response) {
     // handle success
     setData(response.data);
+    //  setChunks(splitArray(response.data));
   })
   .catch(function (error) {
     // handle error
@@ -26,11 +35,27 @@ function getcall() {
 
 }
 
+const splitArray = (temp) => {
+ return _.chunk(temp, 3);
+}
+
+
     return (
-        <div>
-           {data.map((e,i) => <AxiosPropsImageChild data={e} key={i}/>
-            )}
-        <div>
+        <div className="container">
+
+        {chunks.map(e=> (
+           <div className="page-border">
+           {e.map((ele,i) => <AxiosPropsImageChild data={ele} key={i}/>)}
+           </div>
+        )  
+        
+        )}
+
+
+
+           {/* {data.map((e,i) => <AxiosPropsImageChild data={e} key={i}/>
+            )} */}
+        <div >
             {props.e}
         </div>
 
