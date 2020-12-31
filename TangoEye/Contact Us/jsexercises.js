c9d231b17c81c1db4979e6bbe5ff3a8455b93197
selvamani05 <selvamani05@ymail.com>
2020-12-31 22:34:26 +0530

    Jquery-Get-request

--- TangoEye/Contact Us/jsexercises.js
+++ TangoEye/Contact Us/jsexercises.js
@@ -1,14 +1,15 @@
-// 147. Write a JavaScript program to compute the sum of all digits that occur in a given string.  Go to the editor
+// 126. Write a JavaScript program to get the largest even number from an array of integers. Go to the editor
 
-var arr = "1234";
+var arr = [1,2,3,4,5,6];
 
-var n = parseInt(arr);
+var test = []
 
-var res=0;
+var longest = 0;
 
-for(; n!= 0;){
-  res = res + n%10;
-  n = parseInt(n/10);
-}
-
-console.log(res)    
+for(i=0; i < arr.length; i++){
+  if(arr[i] % 2 == 0){
+     if(arr[i] > longest){
+       longest = arr[i];
+     }
+  }
+ }
