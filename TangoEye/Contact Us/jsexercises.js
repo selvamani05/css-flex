c4435dee02c5a23c69068fc56e998d8efd4ee120
selvamani05 <selvamani05@ymail.com>
2021-01-01 21:52:39 +0530

    JS-array-exercises

--- TangoEye/Contact Us/jsexercises.js
+++ TangoEye/Contact Us/jsexercises.js
@@ -1,15 +1,6 @@
-// 126. Write a JavaScript program to get the largest even number from an array of integers. Go to the editor
+// 51. Write a JavaScript program to convert a given number to hours and minutes.  Go to the editor
 
-var arr = [1,2,3,4,5,6];
-
-var test = []
-
-var longest = 0;
-
-for(i=0; i < arr.length; i++){
-  if(arr[i] % 2 == 0){
-     if(arr[i] > longest){
-       longest = arr[i];
-     }
-  }
- }
+var input = 75;
+var hours = parseInt(input / 60);
+var mins = input % 60;
+console.log(hours + 'hours and ' + mins + ' minutes' )
